c07d0a2f2589294ec32e258657e9fd1e89607750
Darryl Wong <darrylsw@gmail.com>
2017-03-09 21:33:07 -0800

    moved classes from models.py to admin.py

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,8 +1,38 @@
+from django.db import models
 from django.contrib import admin
+from django.contrib.auth.models import User
 from myblog.models import Post
 from myblog.models import Category
-from myblog.models import AdminPost
-from myblog.models import AdminCategory
+
+class CategoriesInline(admin.TabularInline):
+    model = Category.posts.through
+
+class AdminPost(admin.ModelAdmin):
+    title = models.CharField(max_length=128)
+    text = models.TextField(blank=True)
+    author = models.ForeignKey(User)
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(blank=True, null=True)
+    inlines = [
+        CategoriesInline,
+    ]
+
+    def __str__(self):
+        return self.title
+
+class AdminCategory(admin.ModelAdmin):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    posts = models.ManyToManyField(Post, blank=True,
+                                   related_name='categories')
+    inlines = [
+        CategoriesInline,
+    ]
+    exclude = ('category', 'posts')
+
+    def __str__(self):
+        return self.name
 
 # Register your models here.
 
--- myblog/models.py
+++ myblog/models.py
@@ -25,32 +25,3 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-class CategoriesInline(admin.TabularInline):
-    model = Category.posts.through
-
-class AdminPost(admin.ModelAdmin):
-    title = models.CharField(max_length=128)
-    text = models.TextField(blank=True)
-    author = models.ForeignKey(User)
-    created_date = models.DateTimeField(auto_now_add=True)
-    modified_date = models.DateTimeField(auto_now=True)
-    published_date = models.DateTimeField(blank=True, null=True)
-    inlines = [
-        CategoriesInline,
-    ]
-
-    def __str__(self):
-        return self.title
-
-class AdminCategory(admin.ModelAdmin):
-    name = models.CharField(max_length=128)
-    description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True,
-                                   related_name='categories')
-    inlines = [
-        CategoriesInline,
-    ]
-    exclude = ('category', 'posts')
-
-    def __str__(self):
-        return self.name
